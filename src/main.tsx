8d0d3390751868bb8b6ce63d79d11068fcf1a56d
unknown <oppongcoffie27@gmail.com>
2026-05-19 15:42:13 +0000

    page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
-import Website from './pages/Website';
+import CountPage from './pages/CountPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <Website/>
+    <CountPage/>
   </StrictMode>,
 )
